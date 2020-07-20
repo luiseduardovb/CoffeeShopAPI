d9fca01a7a7e22dd79aecddd46a3792c2bd11839
luiseduardovb <le_velasquez@yahoo.com>
2020-07-20 19:12:08 +0300

    added Coffee Create

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 //Data
 let coffees = require("./coffees");
-
-const cors = require("cors");
 
 //Create Express App instance
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
+//Coffee List
 app.get("/coffees", (req, res) => {
   res.json(coffees);
 });
 
-//Coffee Delete
+//Coffee Create
+app.post("/coffees", (req, res) => {
+  const id = coffees[coffees.length - 1].id + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newCoffee = { id, slug, ...req.body };
+  coffees.push(newCoffee);
 
+  res.status(201).json(newCoffee);
+});
+
+//Coffee Delete
 app.delete("/coffees/:coffeeId", (req, res) => {
   const { coffeeId } = req.params;
 
