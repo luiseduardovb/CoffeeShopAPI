e315e5e2015652d89dc3a70d2f6f2dba2a1a0c69
luiseduardovb <le_velasquez@yahoo.com>
2020-07-23 19:28:10 +0300

    modals, create Method, List Method, Update Method, Delete Method, Validation

--- controllers/coffeeControlers.js
+++ controllers/coffeeControlers.js
@@ -1,41 +1,56 @@
 const slugify = require("slugify");
 
 //Data
-let coffees = require("../coffees");
+const { Coffee } = require("../db/models");
 
-exports.coffeeList = (req, res) => {
-  res.json(coffees);
-};
-
-exports.coffeeCreate = (req, res) => {
-  const id = coffees[coffees.length - 1].id + 1;
-  const slug = slugify(req.body.name, { lower: true });
-  const newCoffee = { id, slug, ...req.body };
-  coffees.push(newCoffee);
-
-  res.status(201).json(newCoffee);
-};
-
-exports.coffeeUpdate = (req, res) => {
-  const { coffeeId } = req.params;
-  const foundCoffee = coffees.find((coffee) => coffee.id === +coffeeId);
-  if (foundCoffee) {
-    for (const key in req.body) foundCoffee[key] = req.body[key];
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: "Coffee not found" });
+exports.coffeeList = async (req, res) => {
+  try {
+    const coffees = await Coffee.findAll({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    res.json(coffees);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
-exports.coffeeDelete = (req, res) => {
-  const { coffeeId } = req.params;
+exports.coffeeCreate = async (req, res) => {
+  try {
+    const newCoffee = await Coffee.create(req.body);
+    res.status(201).json(newCoffee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-  const foundCoffee = coffees.find((coffee) => coffee.id === +coffeeId);
+exports.coffeeUpdate = async (req, res) => {
+  try {
+    const { coffeeId } = req.params;
+    const foundCoffee = await Coffee.findByPk(coffeeId);
 
-  if (foundCoffee) {
-    coffees = coffees.filter((coffee) => coffee.id !== +coffeeId);
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: "Coffee not found" });
+    if (foundCoffee) {
+      await foundCoffee.update(req.body);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Coffee not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.coffeeDelete = async (req, res) => {
+  try {
+    const { coffeeId } = req.params;
+    const foundCoffee = await Coffee.findByPk(coffeeId);
+
+    if (foundCoffee) {
+      await foundCoffee.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Coffee not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- db/models/Coffee.js
+++ db/models/Coffee.js
@@ -7,15 +7,25 @@ Coffee.init(
   {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     description: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.INTEGER,
+      defaultValue: 1,
+      validate: {
+        min: {
+          args: 1,
+          msg: "Price must be greater or equal to 1",
+        },
+      },
     },
     image: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   },
   {
