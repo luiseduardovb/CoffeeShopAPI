a7a920bdf6b704b9fc5e68d621b495078da758dd
luiseduardovb <le_velasquez@yahoo.com>
2020-08-16 13:08:21 +0300

    db.js new sequelize condition

--- db/db.js
+++ db/db.js
@@ -1,11 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize({
-  username: "postgres",
-  password: "Kuwaithon.2020",
-  database: "coffeebeans_db",
-  dialect: "postgres",
-  host: "localhost",
-});
+const db = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+    })
+  : new Sequelize({
+      username: "postgres",
+      password: "Kuwaithon.2020",
+      database: "coffeebeans_db",
+      dialect: "postgres",
+      host: "localhost",
+    });
 
 module.exports = db;
